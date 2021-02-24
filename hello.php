d0eb835eecd38dabc6c880267d677aa46ed04b23
AndreanBagusSaputra <saputraandreanbagus@gmail.com>
2021-02-24 15:46:15 +0700

    belajar php dasar

---
+++ hello.php
@@ -0,0 +1,24 @@
+<?php
+//echo '<h1>selamat belajar php</h1>';
+//$nama = 'Andrean';
+//$umur = 18;
+//$berat = 75.6;
+//echo '<br/> Nama Saya : ' . $nama;
+//echo '<br/>Umur : ' . $umur. ' tahun';
+//echo '<br/>Berat : ' . $berat. ' Kg';
+
+//<form method="GET" action="form_nilai.php">
+//Nama : <input type="text" name="nama" value="" size="30"/><br/>
+//ata Kuliah :
+
+$angka = 1;
+$nama = "andre";
+$angka_desimal = 3.5;
+define("phi",3.14);
+$jari_jari =7;
+$luas_lingkaran = phi * $jari_jari * $jari_jari;
+
+echo $luas_lingkaran;
+echo "<br/>" .$angka_desimal;
+echo '<br/> nama saya: ' . $nama;
+?>
